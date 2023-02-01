03ab0acb117857c3b151e1a63a1b663d6eb62ff1
User <User@10.0.0.70>
2023-02-01 17:37:10 -0500

    date

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Salman Ibrahim
+ * @author Salman Ibrahim 01/02/2023
  */
 public class CardTrick {
     
